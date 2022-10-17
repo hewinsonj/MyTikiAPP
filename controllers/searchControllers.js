b23061dfdef5886f8868cb40a455208c16594df4
hewinsonj <hewinsonj@gmail.com>
2022-10-17 19:24:58 -0400

    it's not dry yet but it's done

--- controllers/searchControllers.js
+++ controllers/searchControllers.js
@@ -36,7 +36,6 @@ router.get("/search", (req, res) => {
     req.body.inlineCheckbox5 = req.body.inlineCheckbox5 === 'on' ? true : false
     req.body.inlineCheckbox6 = req.body.inlineCheckbox6 === 'on' ? true : false
     req.body.inlineCheckbox7 = req.body.inlineCheckbox7 === 'on' ? true : false
-    req.body.inlineCheckbox8 = req.body.inlineCheckbox8 === 'on' ? true : false
     const checkBox1 = req.body.inlineCheckbox1
     const checkBox2 = req.body.inlineCheckbox2
     const checkBox3 = req.body.inlineCheckbox3
@@ -44,91 +43,107 @@ router.get("/search", (req, res) => {
     const checkBox5 = req.body.inlineCheckbox5
     const checkBox6 = req.body.inlineCheckbox6
     const checkBox7 = req.body.inlineCheckbox7
-    const checkBox8 = req.body.inlineCheckbox8
     console.log(req.body)
+
     if(checkBox1 == true) {
-        TikiDrink.find({name: {$regex: searchItem, $options: "i"}})
-            //console.log("this is the search data ---->", searchItem)
-            .populate("comments.author", "username")
-            .then(tikiDrink => {
-                res.render('search', {tikiDrink, username, loggedIn, userId, searchItem})
-            })
-            .catch(err => res.redirect(`/error?error=${err}`))
+        TikiDrink.find({ $or:[
+            {$and: [{name: {$regex: searchItem, $options: "i"}}, {$or: [{owner: null}, {owner:req.session.userId}]}]}
+        ]})
+        .populate("comments.author", "username")
+        .then(tikiDrink => {
+            res.render('search', {tikiDrink, username, loggedIn, userId, searchItem})
+        })
+        .catch(err => res.redirect(`/error?error=${err}`))
+
     } else if(checkBox2 == true) {
-        TikiDrink.find({ingredients: {$regex: searchItem, $options: "i"}})
-            //console.log("this is the search data ---->", searchItem)
-            .populate("comments.author", "username")
-            .then(tikiDrink => {
-                res.render('search', {tikiDrink, username, loggedIn, userId, searchItem})
-            })
-            .catch(err => res.redirect(`/error?error=${err}`))
+        TikiDrink.find({ $or:[
+            {$and: [{ingredients: {$regex: searchItem, $options: "i"}}, {$or: [{owner: null}, {owner:req.session.userId}]}]}
+        ]})
+        .populate("comments.author", "username")
+        .then(tikiDrink => {
+            res.render('search', {tikiDrink, username, loggedIn, userId, searchItem})
+        })
+        .catch(err => res.redirect(`/error?error=${err}`))
+
     } else if(checkBox3 == true) {
-        TikiDrink.find({garnishes: {$regex: searchItem, $options: "i"}})
-            //console.log("this is the search data ---->", searchItem)
-            .populate("comments.author", "username")
-            .then(tikiDrink => {
-                res.render('search', {tikiDrink, username, loggedIn, userId, searchItem})
-            })
-            .catch(err => res.redirect(`/error?error=${err}`))
+
+        TikiDrink.find({ $or:[
+            {$and: [{garnishes: {$regex: searchItem, $options: "i"}}, {$or: [{owner: null}, {owner:req.session.userId}]}]}
+        ]})
+        .populate("comments.author", "username")
+        .then(tikiDrink => {
+            res.render('search', {tikiDrink, username, loggedIn, userId, searchItem})
+        })
+        .catch(err => res.redirect(`/error?error=${err}`))
 
     } else if(checkBox4 == true) {
-        TikiDrink.find({ogCreator: {$regex: searchItem, $options: "i"}})
-            //console.log("this is the search data ---->", searchItem)
-            .populate("comments.author", "username")
-            .then(tikiDrink => {
-                res.render('search', {tikiDrink, username, loggedIn, userId, searchItem})
-            })
-            .catch(err => res.redirect(`/error?error=${err}`))
+
+        TikiDrink.find({ $or: [
+            {$and: [{ogCreator: {$regex: searchItem, $options: "i"}}, {$or: [{owner: null}, {owner:req.session.userId}]}]}
+        ]})
+        .populate("comments.author", "username")
+        .then(tikiDrink => {
+            res.render('search', {tikiDrink, username, loggedIn, userId, searchItem})
+        })
+        .catch(err => res.redirect(`/error?error=${err}`))
 
     } else if(checkBox5 == true) {
-        TikiDrink.find({lastUpdated: {$regex: searchItem, $options: "i"}})
-            //console.log("this is the search data ---->", searchItem)
-            .populate("comments.author", "username")
-            .then(tikiDrink => {
-                res.render('search', {tikiDrink, username, loggedIn, userId, searchItem})
-            })
-            .catch(err => res.redirect(`/error?error=${err}`))
+        TikiDrink.find({ $or:[
+            {$and: [{lastUpdated: {$regex: searchItem, $options: "i"}}, {$or: [{owner: null}, {owner:req.session.userId}]}]}
+        ]})
+        .populate("comments.author", "username")
+        .then(tikiDrink => {
+            res.render('search', {tikiDrink, username, loggedIn, userId, searchItem})
+        })
+        .catch(err => res.redirect(`/error?error=${err}`))
 
     } else if(checkBox6 == true) {
-        TikiDrink.find({glassware: {$regex: searchItem, $options: "i"}})
-            //console.log("this is the search data ---->", searchItem)
-            .populate("comments.author", "username")
-            .then(tikiDrink => {
-                res.render('search', {tikiDrink, username, loggedIn, userId, searchItem})
-            })
-            .catch(err => res.redirect(`/error?error=${err}`))
+
+        TikiDrink.find({ $or:[
+            {$and: [{glassware: {$regex: searchItem, $options: "i"}}, {$or: [{owner: null}, {owner:req.session.userId}]}]}
+        ]})
+        .populate("comments.author", "username")
+        .then(tikiDrink => {
+            res.render('search', {tikiDrink, username, loggedIn, userId, searchItem})
+        })
+        .catch(err => res.redirect(`/error?error=${err}`))
 
     } else if(checkBox7 == true) {
-        TikiDrink.find({prepInstructs: {$regex: searchItem, $options: "i"}})
-            //console.log("this is the search data ---->", searchItem)
-            .populate("comments.author", "username")
-            .then(tikiDrink => {
-                res.render('search', {tikiDrink, username, loggedIn, userId, searchItem})
-            })
-            .catch(err => res.redirect(`/error?error=${err}`))
-    } else if(checkBox8 == true) {
-        TikiDrink.find({comments: {note:{$regex: searchItem, $options: "i"}}})
-            //console.log("this is the search data ---->", searchItem)
-            .populate("comments.author", "username")
-            .then(tikiDrink => {
-                res.render('search', {tikiDrink, username, loggedIn, userId, searchItem})
-            })
-            .catch(err => res.redirect(`/error?error=${err}`))
+
+        TikiDrink.find({ $or: [
+            {$and: [{prepInstructs: {$regex: searchItem, $options: "i"}}, {$or: [{owner: null}, {owner:req.session.userId}]}]}
+        ]})
+        .populate("comments.author", "username")
+        .then(tikiDrink => {
+            res.render('search', {tikiDrink, username, loggedIn, userId, searchItem})
+        })
+        .catch(err => res.redirect(`/error?error=${err}`))
 
     } else {
-    TikiDrink.find({$or:[{ingredients: {$regex: searchItem, $options: "i"}},  
-    {name: {$regex: searchItem, $options: "i"}}, {garnishes: {$regex: searchItem, $options: "i"}}, {prepInstructs: {$regex: searchItem, $options: "i"}}, {glassware: {$regex: searchItem, $options: "i"}}, {lastUpdated: {$regex: searchItem, $options: "i"}}, {ogCreator: {$regex: searchItem, $options: "i"}}]})
-        //console.log("this is the search data ---->", searchItem)
+
+    TikiDrink.find({$or:[
+        {$and: [{ingredients: {$regex: searchItem, $options: "i"}}, {$or: [{owner: null}, {owner:req.session.userId}]}]},
+        
+        {$and: [{name: {$regex: searchItem, $options: "i"}}, {$or: [{owner: null}, {owner:req.session.userId}]}]}, 
+
+        {$and: [{garnishes: {$regex: searchItem, $options: "i"}}, {$or: [{owner: null}, {owner:req.session.userId}]}]}, 
+        
+        {$and: [{prepInstructs: {$regex: searchItem, $options: "i"}}, {$or: [{owner: null}, {owner:req.session.userId}]}]}, 
+
+        {$and: [{glassware: {$regex: searchItem, $options: "i"}}, {$or: [{owner: null}, {owner:req.session.userId}]}]}, 
+        
+        {$and: [{lastUpdated: {$regex: searchItem, $options: "i"}}, {$or: [{owner: null}, {owner:req.session.userId}]}]}, 
+        
+        {$and: [{ogCreator: {$regex: searchItem, $options: "i"}}, {$or: [{owner: null}, {owner:req.session.userId}]}]}
+    ]})
+
         .populate("comments.author", "username")
         .then(tikiDrink => {
             res.render('search', {tikiDrink, username, loggedIn, userId, searchItem})
         })
         .catch(err => res.redirect(`/error?error=${err}`))
     }
-
 })
-
-
 
 
 //////////////////////////////////////////
--- controllers/tikiDrinkControllers.js
+++ controllers/tikiDrinkControllers.js
@@ -11,10 +11,9 @@ const router = express.Router()
 /////////////////////////////////////////////
 // ROUTES
 /////////////////////////////////////////////
-/// '/mine' is where "MyOriginals" will live. 
 
 // GET request
-///index route
+///index for all tikiDrink route
 router.get("/", (req, res) => {
     const loggedIn = req.session.loggedIn
     if(loggedIn){
@@ -33,10 +32,8 @@ router.get("/", (req, res) => {
     }
 })
 
-
-
 // GET for new tikiDrinks
-// renders the form to create a drink
+// renders the form to create new a drink
 router.get('/new', (req, res) => {
     const username = req.session.username
     const loggedIn = req.session.loggedIn
@@ -48,18 +45,9 @@ router.get('/new', (req, res) => {
     }
 })
 
-
 // POST request
 // create route -> gives the ability to create new drinks
 router.post("/", (req, res) => {
-    // inside this function, that will be referred to as req.body
-    // this is going to add ownership, via a foreign key reference, to our drinks
-    // basically, all we have to do, is append our request body, with the `owner` field, 
-    //and set the value to the logged in user's id
-    // const id = req.params.id
-    // const username = req.session.username
-    // const loggedIn = req.session.loggedIn
-    // const userId = req.session.userId
     req.body.fav = req.body.fav === 'on' ? true : false
     req.body.owner = req.session.userId
     const ingredArr = req.body.ingredients.split(",")
@@ -68,30 +56,20 @@ router.post("/", (req, res) => {
     req.body.garnishes = garnishArr
     const prepArr = req.body.prepInstructs.split(",")
     req.body.prepInstructs = prepArr
-    console.log('the NEW tikiDrink', req.body)
-    // we'll use the mongoose model method `create` to make a new drink
     TikiDrink.create(req.body)
-        // .then(()=> {
-        //     TikiDrink.findById(id)
-   
-        // })
         .then(tikiDrink=> {
             res.redirect('/tikiDrink')
         })
         .catch(err => res.redirect(`/error?error=${err}`))
 })
 
-
 // GET request
 // only drinks owned by logged in user
-// we're going to build another route, that is owner specific, to list all the drinks owned by a certain(logged in) user
 router.get('/mine', (req, res) => {
     // find the drinks, by ownership
     const loggedIn = req.session.loggedIn
     if(loggedIn){
     TikiDrink.find({ owner: req.session.userId })
-        // we want to adjust req.body so that the author is automatically assigned
-    // then display the drinks
         .then(tikiDrink => {
             const username = req.session.username
             const loggedIn = req.session.loggedIn
@@ -102,7 +80,6 @@ router.get('/mine', (req, res) => {
                 res.redirect(`/error?error=must%20log%20in%20to%20continue`)
             }
         })
-    // or throw an error if there is one
         .catch(err => res.redirect(`/error?error=${err}`))
     } else {
         res.redirect(`/error?error=must%20log%20in%20to%20continue`)
@@ -111,32 +88,26 @@ router.get('/mine', (req, res) => {
 
 // GET request
 // only drinks FAVORITED by user
-// we're going to build another route, that is owner specific, to list all the drinks owned by a certain(logged in) user
 router.get('/favs', (req, res) => {
     // find the drinks, by favorship
     const loggedIn = req.session.loggedIn
     if(loggedIn){
     TikiDrink.find({ fav: true, owner: req.session.userId })
-    // then display the drinks
         .then(tikiDrink => {
             const username = req.session.username
             const loggedIn = req.session.loggedIn
             const userId = req.session.userId
             res.render('tikiDrink/index', { tikiDrink, username, loggedIn, userId })
         })
-    // or throw an error if there is one
         .catch(err => res.redirect(`/error?error=${err}`))
     }else{
         res.redirect(`/error?error=must%20log%20in%20to%20continue`)
     }
 })
 
-
 // FAVS PUT request
 // update route -> updates a specific drink
 router.put("/fav/:id", (req, res) => {
-    
-    console.log("req.body initially", req.body)
     const id = req.params.id
      req.body.fav = req.body.fav === 'on' ? true : false
     TikiDrink.findById(id)
@@ -146,7 +117,6 @@ router.put("/fav/:id", (req, res) => {
         .then(() => {
             res.redirect(`/tikiDrink/${id}`)
         })
-
         .catch(err => res.redirect(`/error?error=${err}`))
 })
 
@@ -160,6 +130,8 @@ router.get("/edit/:id", (req, res) => {
     TikiDrink.findById(tikiDrinkId)
     .populate("comments.author", "username")
         // render the edit form if there is a drink
+        //this will join all items in the arrays and add a comma
+        //in between them
         .then(tikiDrink => {
             let ingredientString = ""
             for(let i = 0; i < tikiDrink.ingredients.length; i ++){
@@ -167,8 +139,7 @@ router.get("/edit/:id", (req, res) => {
                     ingredientString += tikiDrink.ingredients[i]
                 } else {
                     ingredientString += tikiDrink.ingredients[i] + ","
-                }
-                
+                }    
             }
             let garnishString = ""
             for(let i = 0; i < tikiDrink.garnishes.length; i ++){
@@ -177,7 +148,6 @@ router.get("/edit/:id", (req, res) => {
                 } else {
                     garnishString += tikiDrink.garnishes[i] + ","
                 }
-                
             }
             let prepInstructString = ""
             for(let i = 0; i < tikiDrink.prepInstructs.length; i ++){
@@ -185,17 +155,13 @@ router.get("/edit/:id", (req, res) => {
                     prepInstructString += tikiDrink.prepInstructs[i]
                 } else {
                     prepInstructString += tikiDrink.prepInstructs[i] + ","
-                }
-                
+                }   
             }
-            console.log(ingredientString, "<--------")
             res.render('tikiDrink/edit', { tikiDrink, username, loggedIn, userId, ingredientString, garnishString, prepInstructString })
         })
-        // redirect if there isn't
         .catch(err => {
             res.redirect(`/error?error=${err}`)
         })
-    // res.send('edit page')
     }else{
         res.redirect(`/error?error=must%20log%20in%20to%20continue`)
     }
@@ -209,13 +175,13 @@ router.put("/:id", (req, res) => {
     const loggedIn = req.session.loggedIn
     req.body.fav = req.body.fav === 'on' ? true : false
     req.body.owner = req.session.userId
+    //this will split each string at every comma
     const ingredArr = req.body.ingredients.split(",")
     req.body.ingredients = ingredArr
     const garnishArr = req.body.garnishes.split(",")
     req.body.garnishes = garnishArr
     const prepArr = req.body.prepInstructs.split(",")
     req.body.prepInstructs = prepArr
-
     console.log('req.body after changing checkbox value', req.body)
     if(loggedIn){
     TikiDrink.findById(id)
@@ -256,7 +222,6 @@ router.get("/customize/:id", (req, res) => {
         } else {
             ingredientString += tikiDrink.ingredients[i] + ","
         }
-        
     }
     let garnishString = ""
     for(let i = 0; i < tikiDrink.garnishes.length; i ++){
@@ -264,8 +229,7 @@ router.get("/customize/:id", (req, res) => {
             garnishString += tikiDrink.garnishes[i]
         } else {
             garnishString += tikiDrink.garnishes[i] + ","
-        }
-        
+        }   
     }
     let prepInstructString = ""
     for(let i = 0; i < tikiDrink.prepInstructs.length; i ++){
@@ -277,22 +241,17 @@ router.get("/customize/:id", (req, res) => {
     }
             res.render('tikiDrink/customize', { tikiDrink, username, loggedIn, userId, ingredientString, garnishString, prepInstructString })
         })
-        // redirect if there isn't
         .catch(err => {
             res.redirect(`/error?error=${err}`)
         })
-    // res.send('edit page')
 })
 
 ///// DELETE Request
 router.delete('/:id', (req, res) => {
-    // get the fruit id
     const tikiDrinkId = req.params.id
-
     // delete and REDIRECT
     TikiDrink.findByIdAndRemove(tikiDrinkId)
         .then(tikiDrink => {
-            // if the delete is successful, send the user back to the index page
             res.redirect('/tikiDrink/mine')
         })
         .catch(err => {
@@ -305,12 +264,6 @@ router.delete('/:id', (req, res) => {
 router.get("/:id", (req, res) => {
     const id = req.params.id
     TikiDrink.findById(id)
-        
-        // populate will provide more data about the document that is in the specified collection
-        // the first arg is the field to populate
-        // the second can specify which parts to keep or which to remove
-        //.populate("owner", "username")
-        // we can also populate fields of our subdocuments
         .populate("comments.author", "username")
         
         .then(tikiDrink => {
@@ -327,12 +280,6 @@ router.get("/:id", (req, res) => {
 router.get("/deleteConf/:id", (req, res) => {
     const id = req.params.id
     TikiDrink.findById(id)
-        
-        // populate will provide more data about the document that is in the specified collection
-        // the first arg is the field to populate
-        // the second can specify which parts to keep or which to remove
-        //.populate("owner", "username")
-        // we can also populate fields of our subdocuments
         .populate("comments.author", "username")
         
         .then(tikiDrink => {
@@ -344,8 +291,6 @@ router.get("/deleteConf/:id", (req, res) => {
         })
         .catch(err => res.redirect(`/error?error=${err}`))
 })
-
-
 
 //////////////////////////////////////////
 // Export the Router
